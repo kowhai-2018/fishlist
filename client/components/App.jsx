b12c562fb4e390229a0323e2fa478c91de281934
Rich Churcher <rich.churcher@gmail.com>
2018-11-28 18:55:50 +1300

    More progress

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,10 +1,8 @@
 import React from 'react'
 
-const App = () => {
-  return (
-    <h1>React development has begun!</h1>
-  )
-}
+import FishList from './FishList'
+
+const App = () => <FishList />
 
 export default App
 
