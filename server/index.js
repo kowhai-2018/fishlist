ccad5410e1d284f4711a28e1395e7a141c75673a
Rich Churcher <rich.churcher@gmail.com>
2018-11-30 09:21:25 +1300

    Whoops. Remove debug testing code!

--- server/index.js
+++ server/index.js
@@ -4,9 +4,4 @@ const server = require('./server')
 
 const port = process.env.PORT || 3000
 
-function fleef () {
-  const meep = 2
-}
-
-fleef()
 server.listen(port, () => console.log(`Listening on ${port}`))
