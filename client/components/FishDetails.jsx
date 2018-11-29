803a767ff20680acdbad9943572fd1a171d7b681
Jeremy-Jonas <jeremy.jonas@hotmail.co.nz>
2018-11-29 15:12:37 +1300

    Removed redundant code

--- client/components/FishDetails.jsx
+++ client/components/FishDetails.jsx
@@ -2,22 +2,6 @@ import React from 'react'
 import {Container, Grid, Icon, Image, Header, Divider, Button, Placeholder} from 'semantic-ui-react'
 import {Link} from 'react-router-dom'
 
-const style = {
-  h1: {
-    marginTop: '3em'
-  },
-  h2: {
-    margin: '4em 0em 2em'
-  },
-  h3: {
-    marginTop: '2em',
-    padding: '2em 0em'
-  },
-  last: {
-    marginBottom: '300px'
-  }
-}
-
 export default function FishDetails () {
   return (
     <div>
