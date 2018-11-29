00accf23f75696efb49162cbe7f086a21d8e27f1
Jeremy-Jonas <jeremy.jonas@hotmail.co.nz>
2018-11-29 14:16:32 +1300

    Created route for individual fish details

---
+++ client/components/FishDetails.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function FishDetails () {
+  return (
+    <div>
+      <button>Back</button> Paua <img src='https://placekitten.com/200/300'></img>
+      <p>Lorem</p>
+      <a href='google.com/paua'>More info</a>
+      <h3>Media Box & large Img</h3>
+    </div>
+  )
+}
--- knexfile.js
+++ knexfile.js
@@ -1,26 +1,35 @@
+// Update with your config settings.
+
 module.exports = {
 
   development: {
     client: 'sqlite3',
     connection: {
       filename: './dev.sqlite3'
-    },
-    useNullAsDefault: true
+    }
   },
 
-  test: {
-    client: 'sqlite3',
+  staging: {
+    client: 'postgresql',
     connection: {
-      filename: ':memory:'
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
     },
-    useNullAsDefault: true
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
   },
 
   production: {
     client: 'postgresql',
     connection: {
       database: 'my_db',
-      user: 'username',
+      user:     'username',
       password: 'password'
     },
     pool: {
@@ -32,4 +41,4 @@ module.exports = {
     }
   }
 
-}
+};
