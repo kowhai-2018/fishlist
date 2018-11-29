b0afcba9de4aa894157d0b3b6c417c7a4cc671c7
Kiri Tavaga <kiri.tavaga@icloud.com>
2018-11-29 16:03:15 +1300

    add new form working on path

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { Route, BrowserRouter as Router, Switch } from 'react-router-dom'
 import { Container } from 'semantic-ui-react'
 
+import AddNew from './AddNew'
 import FishList from './FishList'
 import TopMenu from './TopMenu'
 
@@ -14,6 +15,7 @@ class App extends React.Component {
           <Container style={{ marginTop: 75 }}>
             <Switch>
               <Route exact path='/' component={FishList} />
+              <Route path='/new' component={AddNew} />
             </Switch>
           </Container>
         </React.Fragment>
