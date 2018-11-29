0d4a2e2028629190da251136ba55e2c51d079c7e
Jeremy-Jonas <jeremy.jonas@hotmail.co.nz>
2018-11-29 16:16:18 +1300

    Set unique property on 'name' column in fish table

--- migrations/20181128102151_fish.js
+++ migrations/20181128102151_fish.js
@@ -1,7 +1,7 @@
 exports.up = knex =>
   knex.schema.createTable('fish', t => {
     t.increments('id').primary()
-    t.string('name')
+    t.string('name').unique()
     t.timestamps(true, true)
   })
 
