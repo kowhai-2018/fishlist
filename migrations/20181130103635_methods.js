83b21d2476da6645b264ebec9a84f83f932527b3
Kiri Tavaga <kiri.tavaga@icloud.com>
2018-11-30 11:01:00 +1300

    added a method table

--- migrations/20181130103635_methods.js
+++ migrations/20181130103635_methods.js
@@ -1,7 +1,6 @@
 exports.up = knex =>
   knex.schema.createTable('methods', t => {
     t.increments('id').primary()
-    t.integer('method_id').references('methods.id')
     t.string('method')
     t.timestamps(true, true)
   })
