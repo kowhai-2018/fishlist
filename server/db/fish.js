a69e5faceb548c1535764ee2e67f077a6809bff6
Chris <chrislaupama@icloud.com>
2018-11-30 11:52:44 +1300

    modified the create fish function to update the other db's as well

--- server/db/fish.js
+++ server/db/fish.js
@@ -1,16 +1,21 @@
 const connection = require('./')
 
-function get (db = connection) {
+function get(db = connection) {
   return db('fish')
 }
 
-function createFish (newFish, fishDetail, db = connection) {
+function createFish(
+  newFish,
+  fishDetail,
+  fishLevel,
+  fishMethod,
+  db = connection
+) {
   return db('fish')
-  .insert(newFish)
-  .then(
-    db('fish_details')
-    .insert(fishDetail)
-  )
+    .insert(newFish)
+    .then(db('fish_details').insert(fishDetail))
+    .then(db('levels').insert(fishLevel))
+    .then(db('methods').insert(fishMethod))
 }
 
 module.exports = {
