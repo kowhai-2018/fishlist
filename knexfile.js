123499ffb0a560c72b41e6d541134e8d12cd0fea
Jeremy-Jonas <jeremy.jonas@hotmail.co.nz>
2018-11-29 16:24:50 +1300

    Restored original knexfile from development

--- knexfile.js
+++ knexfile.js
@@ -1,5 +1,3 @@
-// Update with your config settings.
-
 module.exports = {
 
   development: {
@@ -10,27 +8,19 @@ module.exports = {
     useNullAsDefault: true
   },
 
-  staging: {
-    client: 'postgresql',
+  test: {
+    client: 'sqlite3',
     connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
+      filename: ':memory:'
     },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
+    useNullAsDefault: true
   },
 
   production: {
     client: 'postgresql',
     connection: {
       database: 'my_db',
-      user:     'username',
+      user: 'username',
       password: 'password'
     },
     pool: {
@@ -42,4 +32,4 @@ module.exports = {
     }
   }
 
-};
+}
