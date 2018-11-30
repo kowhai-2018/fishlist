243a48bdaf4c4752f387dd306ab92830052f2e54
Dasol Baek <schgana@hotmail.com>
2018-11-30 16:04:39 +1300

    tested if fishDetails database func is working.

--- tests/server/fishDetails.test.js
+++ tests/server/fishDetails.test.js
@@ -10,15 +10,22 @@ beforeEach(() => {
 
 afterEach(() => env.cleanup(testDb))
 
-test('fishDetail.getFishDetail to return details of the given fishId ', () => {
+test('fishDetail.getFishDetail(1) to return details of the given fishId = 1', () => {
   expect.assertions(1)
   const expected = {
-    name:,
-    description,
-    links,
-    image,
-    video
+    name: 'paua',
+    link: 'https://en.wikipedia.org/wiki/P%C4%81ua'
   }
-  const actual = fishDetails.getFishDetail('test', testDb)
+  const actual = fishDetails.getFishDetail(1, testDb)
+  return expect(actual).resolves.toMatchObject(expected)
+})
+
+test('fishDetail.getFishDetail(13) to return details of the given fishId = 13', () => {
+  expect.assertions(1)
+  const expected = {
+    name: 'vegan fish',
+    link: 'https://shop.countdown.co.nz/shop/productdetails?stockcode=743739'
+  }
+  const actual = fishDetails.getFishDetail(13, testDb)
   return expect(actual).resolves.toMatchObject(expected)
 })
