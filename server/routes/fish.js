56e4c3c1b4f96dd7de39ae7e1af4daeba5694ed3
Chris <chrislaupama@icloud.com>
2018-11-30 14:37:11 +1300

    updated post route

--- server/routes/fish.js
+++ server/routes/fish.js
@@ -7,37 +7,36 @@ const fishDetails = require('../db/fishDetails')
 const router = express.Router()
 
 router.get('/', (req, res) => {
-  fish.get()
-    .then(fish => res.status(200).json(fish))
+  fish.get().then(fish => res.status(200).json(fish))
 })
 
 router.get('/:id', (req, res) => {
-  const fishId = (Number(req.params.id))
-  fishDetails.getFishDetail(fishId)
+  const fishId = Number(req.params.id)
+  fishDetails
+    .getFishDetail(fishId)
     .then(fishDetail => res.status(200).json(fishDetail))
 })
 
-router.post('/new', (req, res) => {
+router.post('/', (req, res) => {
+  const { name, levelId, methodId, description, image, links, video } = req.body
   const newFish = {
-    name: req.body.name
+    name,
+    level_id: levelId,
+    method_id: methodId
   }
   const fishDetail = {
-    description: req.body.description,
-    image: req.body.image,
-    links: req.body.links,
-    video: req.body.video
+    description,
+    image,
+    links,
+    video
   }
-  const fishLevel = {
-    level: req.body.level
-  }
-  const fishMethod = {
-    method: req.body.method
-  }
-  fish.createFish(newFish, fishDetail, fishLevel, fishMethod)
-    .then(() => {
-      res.redirect('/')
-    })
-})
 
+  fish
+    .createFish(newFish, fishDetail)
+    .then(() => {
+      res.json({ Okay: true })
+    })
+    .catch((err) => res.json({ Okay: false, error: err.message }))
+})
 
 module.exports = router
