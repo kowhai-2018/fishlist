c732f788f433ce2d872499f99a38cbe52f0d418a
Dasol Baek <schgana@hotmail.com>
2018-12-03 11:01:49 +1300

    test fish action type GET_FISH_DETAIL_SUCCESS returns fish details

--- tests/client/reducers/fishReducer.test.js
+++ tests/client/reducers/fishReducer.test.js
@@ -1,4 +1,5 @@
 import fishReducer from '../../../client/reducer/fish'
+import fishDetailsReducer from '../../../client/reducer/fishDetails'
 
 test('GET_FISH_SUCCESS action type returns action.fish', () => {
   expect.assertions(1)
@@ -11,3 +12,17 @@ test('GET_FISH_SUCCESS action type returns action.fish', () => {
   expect(actual).toBe(expected)
 })
 
+test('GET_FISH_DETAIL_SUCCESS action type returns action.fishDetail', () => {
+  expect.assertions(1)
+  const expected = {
+    name: 'cockle',
+    description: 'cockle is cute',
+    image: 'http:// link to image location'
+  }
+  const action = {
+    type: 'GET_FISH_DETAIL_SUCCESS',
+    fishDetail: expected
+  }
+  const actual = fishDetailsReducer({}, action)
+  expect(actual).toMatchObject(expected)
+})
