5bec972e16382788218013bb88f49c3a24f9c4e7
Jeremy-Jonas <jeremy.jonas@hotmail.co.nz>
2018-12-03 12:28:35 +1300

    created app component test file

---
+++ tests/client/components/App.test.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import {shallow} from 'enzyme'
+
+import App from '../../../client/components/App'
+
+test('<App />', () => {
+  const expected = ''
+  const wrapper = shallow(<App />)
+  expect(wrapper.text()).toMatch(expected)
+})
