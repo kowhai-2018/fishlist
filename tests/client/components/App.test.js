21f53446a7751f8dedc15ff0d976bdb2156ffc26
Jeremy-Jonas <jeremy.jonas@hotmail.co.nz>
2018-12-03 12:53:45 +1300

    Added more tests

--- tests/client/components/App.test.js
+++ tests/client/components/App.test.js
@@ -1,10 +1,31 @@
 import React from 'react'
-import {shallow} from 'enzyme'
+import {configure, shallow} from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16'
 
 import App from '../../../client/components/App'
+import TopMenu from '../../../client/components/TopMenu'
+import FishList from '../../../client/components/FishList'
+
+configure({adapter: new Adapter()})
 
 test('<App />', () => {
   const expected = '<BrowserRouter />'
   const wrapper = shallow(<App />)
   expect(wrapper.text()).toMatch(expected)
+})
+
+test('<App /> renders a <TopMenu /> component', () => {
+  const expected = true
+  const wrapper = shallow(<App />)
+  const actual = wrapper.containsMatchingElement(TopMenu)
+
+  expect(actual).toBe(expected)
+})
+
+test('<App /> renders a <FishList /> component', () => {
+  const expected = true
+  const wrapper = shallow(<App />)
+  const actual = wrapper.containsMatchingElement(FishList)
+
+  expect(actual).toBe(expected)
 })
