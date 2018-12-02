31fbde89bb6b67c6b0266b61fa3c7a55942c7b3b
Bryce Kehoe <bryce@kehoe.co.nz>
2018-12-03 10:38:27 +1300

    Remove test for '/api/v1/auth'

--- tests/server/routes.test.js
+++ tests/server/routes.test.js
@@ -1,4 +1,3 @@
-const index = require('../../server/db/index')
 const server = require('../../server/server')
 const request = require('supertest')
 
@@ -15,16 +14,6 @@ test('/', (done) => {
 test('/:id', (done) => {
   request(server)
     .get('/:id')
-    .expect(200)
-    .end((err, res) => {
-      expect(err).toBeNull()
-      done()
-    })
-})
-
-test('/api/v1/auth', (done) => {
-  request(server)
-    .get('/api/v1/auth')
     .expect(200)
     .end((err, res) => {
       expect(err).toBeNull()
