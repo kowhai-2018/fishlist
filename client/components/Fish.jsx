4ab3a5828549355b354cd793ee4500156308fc6d
Maya Ningsih <echaymaya@gmail.com>
2018-12-04 10:54:18 +1300

    on Fish component:changed function to class, created event handler and updated render section. On FishDetailPop component: created a simple <Popup>

--- client/components/Fish.jsx
+++ client/components/Fish.jsx
@@ -1,19 +1,30 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
 import {Header, List, Label} from 'semantic-ui-react'
+import FishDetailPopup from './FishDetailPopup'
 
-export default function Fish (props) {
-  return (
-    <List.Item size='large'>
-      <Link to={`/fish/${props.fishData.id}`}>
-        <Header>
-          <Label as='a' color={props.fishData.color} size='medium' horizontal>
-            {props.fishData.level}
-          </Label>
-          {props.fishData.name}
-        </Header>
-      </Link>
-    </List.Item>
+export default class Fish extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      showPopup: false
+    }
+  }
+  onHoverHandler = e => this.setState({showPopup:true})
 
-  )
+  render () {
+    return (
+      <List.Item size='large'>
+        <Link to={`/fish/${this.props.fishData.id}`}>
+          <Header>
+            <Label  onMouseOver={this.onHoverHandler} as='a' color={this.props.fishData.color} size='medium' horizontal>
+              {this.props.fishData.level}
+            </Label>
+            {this.props.fishData.name}
+          </Header>
+          {this.state.showPopup && <FishDetailPopup />}
+        </Link>
+      </List.Item>
+    )
+  }
 }
--- client/components/FishDetailPopup.jsx
+++ client/components/FishDetailPopup.jsx
@@ -1,24 +1,29 @@
 import React from 'react'
-import Popup from 'semantic-ui-react'
-import Fish from './Fish'
+import {Popup, Button} from 'semantic-ui-react'
+// import Fish from './Fish'
 
-export class FishDetailPopup extends React.Component {
+export default class FishDetailPopup extends React.Component {
   componentDidMount () {
 
   }
 
   render () {
     return (
-      <div>
-        {this.props.fishData.map(fish => (
-          <Popup
-            key={fish.name}
-            trigger={<Fish key={fish.id} fishData={fish} />}
-            header={fish.name}
-            content={fish.description}
-          />
-        ))}
-      </div>
+      // <div>
+      //   {this.props.fishData.map(fish => (
+      //     <Popup
+      //       key={fish.name}
+      //       trigger={<Fish key={fish.id} fishData={fish} />}
+      //       header={fish.name}
+      //       content={fish.description}
+      //     />
+      //   ))}
+      // </div>
+      <Popup
+        trigger={<Button icon='add' />}
+        content="The default theme's basic popup removes the pointing arrow."
+        basic
+      />
     )
   }
 }
