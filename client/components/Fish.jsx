cb67ea4ddb67f8c85bf0fd49fec9ba3e0022004e
Dasol Baek <schgana@hotmail.com>
2018-12-03 15:06:39 +1300

    changed position of label and link

--- client/components/Fish.jsx
+++ client/components/Fish.jsx
@@ -5,12 +5,14 @@ import {Header, List, Label} from 'semantic-ui-react'
 export default function Fish (props) {
   return (
     <List.Item size='large'>
-      <Header>
-        <Link to={`/fish/${props.fishData.id}`}>{props.fishData.name}</Link>
-        <Label as='a' color={props.fishData.color} size='medium' horizontal>
-          {props.fishData.level}
-        </Label>
-      </Header>
+      <Link to={`/fish/${props.fishData.id}`}>
+        <Header>
+          <Label as='a' color={props.fishData.color} size='medium' horizontal>
+            {props.fishData.level}
+          </Label>
+          {props.fishData.name}
+        </Header>
+      </Link>
     </List.Item>
 
   )
