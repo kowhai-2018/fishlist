66dea5a21176bec546a566de84925282a5774f0a
Maya Ningsih <echaymaya@gmail.com>
2018-12-03 15:46:39 +1300

    started popup

--- client/components/Fish.jsx
+++ client/components/Fish.jsx
@@ -1,9 +1,28 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
 import {Popup} from 'semantic-ui-react'
+import { connect } from 'react-redux'
 
-export default function Fish (props) {
-  return (
-    <li><Link to={`/fish/${props.fishData.id}`}>{props.fishData.name}</Link></li>
-  )
+export class Fish extends React.Component {
+  state = {
+    onhover : ''
+  }
+
+  componentDidMount () {
+    
+  }
+  
+  onHoverHandler() => {
+
+  }
+
+  render() {
+    return (
+      <li>
+        <Popup key={this.state.fishData} header={this.state.fishData.image} content={}/><Link to={`/fish/${props.fishData.id}`}>{props.fishData.name}</Link>
+      </li>
+    )
+  }
 }
+
+export default Fish
