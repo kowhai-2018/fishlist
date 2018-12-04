a8ebd725257ffa5c001af6bfe0fda50105a3c9bb
Maya Ningsih <echaymaya@gmail.com>
2018-12-04 15:19:19 +1300

    Modify get in db/fish.js to return all fish data

--- server/db/fish.js
+++ server/db/fish.js
@@ -2,25 +2,9 @@ const connection = require('./')
 
 function get (db = connection) {
   return db('fish')
-    .join('levels', 'fish.level_id', '=', 'levels.id')
-    .select(
-      'fish.id as id',
-      'fish.name as name',
-      'color',
-      'level',
-      'level_id',
-      'method_id',
-      'fish.created_at',
-      'fish.updated_at'
-    )
-}
-
-function getById (id, db = connection) {
-  return db('fish')
     .join('levels', 'fish.level_id', 'levels.id')
     .leftOuterJoin('methods', 'fish.method_id', 'methods.id')
     .join('fish_details', 'fish.id', 'fish_details.fish_id')
-    .where('fish.id', id)
     .select(
       'fish.id as id',
       'fish.name as name',
@@ -53,6 +37,5 @@ function createFish (
 
 module.exports = {
   get,
-  getById,
   createFish
 }
