28b47cca465761cc4ef86318781bb92eb359ea9a
Rich Churcher <rich.churcher@gmail.com>
2018-12-04 14:59:35 +1300

    Use .text to make Postgres happy

--- migrations/20181129114059_fish_details.js
+++ migrations/20181129114059_fish_details.js
@@ -2,7 +2,7 @@ exports.up = knex =>
   knex.schema.createTable('fish_details', t => {
     t.increments('id').primary()
     t.integer('fish_id').references('fish.id')
-    t.string('description')
+    t.text('description')
     t.string('image')
     t.string('link')
     t.string('video')
