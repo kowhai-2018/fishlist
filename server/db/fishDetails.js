285aaf960ec089150e37280140daaaad2d600075
alliewebb <alliewebb@Allies-MacBook-Pro.local>
2018-12-04 15:03:26 +1300

    commit before exiting

--- server/db/fishDetails.js
+++ server/db/fishDetails.js
@@ -5,7 +5,7 @@ function getFishDetail (fishId, db = connection) {
   return db('fish_details')
     .where('fish_id', fishId)
     .join('fish', 'fish.id', '=', 'fish_details.fish_id')
-    .select('name', 'description', 'link', 'image', 'video')
+    .select('fish.id as id', 'name', 'description', 'link', 'image', 'video')
     .first()
 }
 
