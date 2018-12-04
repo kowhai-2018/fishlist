30d888f0b7849ea83dc46f0f9ad788494cd35b22
Maya Ningsih <echaymaya@gmail.com>
2018-12-04 15:03:18 +1300

    Add getById function to db/fish.js

--- server/db/fish.js
+++ server/db/fish.js
@@ -15,6 +15,29 @@ function get (db = connection) {
     )
 }
 
+function getById (id, db = connection) {
+  return db('fish')
+    .join('levels', 'fish.level_id', 'levels.id')
+    .leftOuterJoin('methods', 'fish.method_id', 'methods.id')
+    .join('fish_details', 'fish.id', 'fish_details.fish_id')
+    .where('fish.id', id)
+    .select(
+      'fish.id as id',
+      'fish.name as name',
+      'color',
+      'level',
+      'level_id',
+      'method_id',
+      'method',
+      'description',
+      'image',
+      'link',
+      'video',
+      'fish.created_at',
+      'fish.updated_at'
+    )
+}
+
 function createFish (
   newFish,
   fishDetail,
@@ -30,5 +53,6 @@ function createFish (
 
 module.exports = {
   get,
+  getById,
   createFish
 }
